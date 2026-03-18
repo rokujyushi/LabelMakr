36677e85fab6e774f3712bafc8e36d3b920ef113
rokujyushi <rosabu376@gmail.com>
2026-03-18 19:25:51 +0900

    install_assets.pyをリファクタリングし、ファイルダウンロード機能を統一。SOFAモデルのインストールプロセスを改善し、エラーハンドリングを追加。

--- install_assets.py
+++ install_assets.py
@@ -1,9 +1,10 @@
-import zipfile
 import os
-import requests
 import logging
-import sys
+import shutil
+import zipfile
 from pathlib import Path as P
+
+import requests
 from tqdm import tqdm
 
 #
@@ -20,13 +21,10 @@ folder = P('./models')
 if not folder.exists():
 	folder.mkdir()
 
-def spicytigermeat_asset():
-	logger.info('SetUp spicytigermeat_assets for LabelMakr.')
-	logger.info('Downloading SOFA models.')
 
-	url = 'https://github.com/spicytigermeat/LabelMakr/releases/download/assets_v030/models.zip'
-	filepath = 'models.zip'
+def download_file(url, filepath):
 	r = requests.get(url, stream=True)
+	r.raise_for_status()
 
 	total_size = int(r.headers.get('content-length', 0))
 	block_size = 1024
@@ -34,11 +32,50 @@ def spicytigermeat_asset():
 	with tqdm(total=total_size, unit='B', unit_scale=True) as pbar:
 		with open(filepath, 'wb') as file:
 			for data in r.iter_content(block_size):
+				if not data:
+					continue
 				pbar.update(len(data))
 				file.write(data)
 
 	if total_size != 0 and pbar.n != total_size:
 		raise RuntimeError('Could not download file.')
+
+
+def install_sofa_asset():
+	logger.info('SetUp SOFA for LabelMakr.')
+	filepath = 'SOFA-main.zip'
+	extract_dir = P('./_sofa_extract')
+
+	if extract_dir.exists():
+		shutil.rmtree(extract_dir)
+
+	url = 'https://github.com/qiuqiao/SOFA/archive/refs/heads/main.zip'
+	download_file(url, filepath)
+	extract_dir.mkdir(exist_ok=True)
+
+	logger.info('Unzipping SOFA...')
+	with zipfile.ZipFile(filepath, 'r') as archive:
+		archive.extractall(extract_dir)
+
+	extracted = [path for path in extract_dir.iterdir() if path.is_dir()]
+	if not extracted:
+		raise RuntimeError('SOFA archive could not be extracted.')
+
+	if P('./SOFA').exists():
+		shutil.rmtree('./SOFA')
+
+	shutil.move(str(extracted[0]), './SOFA')
+	shutil.rmtree(extract_dir)
+	os.remove(filepath)
+	logger.info('Done setting up SOFA for LabelMakr.')
+
+def spicytigermeat_asset():
+	logger.info('SetUp spicytigermeat_assets for LabelMakr.')
+	logger.info('Downloading SOFA models.')
+
+	url = 'https://github.com/spicytigermeat/LabelMakr/releases/download/assets_v030/models.zip'
+	filepath = 'models.zip'
+	download_file(url, filepath)
 
 	logger.info('Sucessfully donwloaded models.')
 	logger.info('Unzipping...')
@@ -56,17 +93,7 @@ def add_some_assets():
 	url = 'https://github.com/Greenleaf2001/SOFA_Models/releases/tag/JPN_Test2_Plus'
 	files = ['hparams.yaml','step.100000.ckpt','japanese-extension-sofa.txt']
 	for file in files:
-		r = requests.get(f'{url}/{file}', stream=True)
-
-		total_size = int(r.headers.get('content-length', 0))
-		block_size = 1024
-		with tqdm(total=total_size, unit='B', unit_scale=True) as pbar:
-			with open(file, 'wb') as f:
-				for data in r.iter_content(block_size):
-					pbar.update(len(data))
-					f.write(data)
-		if total_size != 0 and pbar.n != total_size:
-			raise RuntimeError('Could not download file.')
+		download_file(f'{url}/{file}', file)
 	
 	logger.info('Sucessfully donwloaded models.')
 	logger.info('Moving files...')
@@ -97,16 +124,7 @@ def JP_g2p_asset():
 
 	url = 'https://github.com/CjangCjengh/japanese_g2p/releases/download/v1.0.0/japanese_g2p.zip'
 	filepath = 'japanese_g2p.zip'
-	r = requests.get(url, stream=True)
-	total_size = int(r.headers.get('content-length', 0))
-	block_size = 1024
-	with tqdm(total=total_size, unit='B', unit_scale=True) as pbar:
-		with open(filepath, 'wb') as file:
-			for data in r.iter_content(block_size):
-				pbar.update(len(data))
-				file.write(data)
-	if total_size != 0 and pbar.n != total_size:
-		raise RuntimeError('Could not download file.')
+	download_file(url, filepath)
 	logger.info('Sucessfully donwloaded models.')
 	logger.info('Unzipping...')
 	with zipfile.ZipFile(filepath, 'r') as archive:
@@ -114,8 +132,7 @@ def JP_g2p_asset():
 	os.rename('japanese_g2p', f'g2p-jp')
 	os.remove(filepath)
 	logger.info('Done setting up JP_g2p_asset for LabelMakr.')
-	
-
+install_sofa_asset()
 spicytigermeat_asset()
 add_some_assets()
 JP_g2p_asset()
